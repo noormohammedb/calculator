da3e74230b79ef61765262719cd2c5901a0628d1
Noor Mohammed B <nooru.mohad@gmail.com>
2020-03-14 16:41:45 +0530

    script updated

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 console.log('JavaScript Running');
 $(function(){
     console.log("jQuery Running");
-})
 
-// $(#screen).html("jfjfjf");
+    $("#history").val("his");
+
+    
+
+    
+  
+    
+
+
+})
+// var his;
+function display(res){
+    var his = $("#screen").val();
+    $("#screen").val(res);
+    console.log("result print" + res);
+    $("#history").val(his);
+    console.log("history print " + his);
+}
